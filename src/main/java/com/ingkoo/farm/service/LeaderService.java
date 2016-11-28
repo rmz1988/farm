ae958e1a4d1c7c90231a4aa21233243983436717
rmz1988 <rmz1988@126.com>
2016-11-28 13:21:16 +0800

    修改互转和提现的限制

--- src/main/java/com/ingkoo/farm/service/LeaderService.java
+++ src/main/java/com/ingkoo/farm/service/LeaderService.java
@@ -34,12 +34,12 @@ public class LeaderService {
 				//存在上级，则计算领导奖
 				if (StringUtils.isNotEmpty(user.getStr("recommendUserId"))) {
 					User leaderUser = User.dao.findById(user.getStr("recommendUserId"));
-					if (!moneyService.isOverDailyIncome(leaderUser.getStr("userId"))) {
-						//计算当代用户应获得的提成
-						LeaderRate leaderRate =
-								generation > 7 ? LeaderRate.dao.findById(999) : LeaderRate.dao.findById(generation);
-						String actualIncome =
-								new Money(dailyOutput).multiply(leaderRate.getInt("rate")).divide(100).toString();
+					//if (!moneyService.isOverDailyIncome(leaderUser.getStr("userId"))) {
+					//计算当代用户应获得的提成
+					LeaderRate leaderRate =
+							generation > 7 ? LeaderRate.dao.findById(999) : LeaderRate.dao.findById(generation);
+					String actualIncome =
+							new Money(dailyOutput).multiply(leaderRate.getInt("rate")).divide(100).toString();
 
 						/*//用户余额和今日收益增加相应提成
 						leaderUser.set("money", new Money(leaderUser.getStr("money")).add(actualIncome).toString())
@@ -48,18 +48,18 @@ public class LeaderService {
 								.update();
 						//记录用户领导奖收益记录
 						TotalIncome.dao.saveLeaderIncome(leaderUser, actualIncome);*/
-						if (!actualIncome.equals("0.00")) {
-							//记录领导奖收益
-							//						new LeaderIncome().set("userId", leaderUser.getStr("userId"))
-							//								.set("money", actualIncome)
-							//								.set("createTime", DateUtils.format(new Date(), DateTimeConst.DATE_10)).save();
-							Db.update("insert into leader_income values (null,?,?,?)", leaderUser.getStr("userId"),
-									actualIncome, DateUtils.format(new Date(), DateTimeConst.DATE_10));
+					if (!actualIncome.equals("0.00")) {
+						//记录领导奖收益
+						//						new LeaderIncome().set("userId", leaderUser.getStr("userId"))
+						//								.set("money", actualIncome)
+						//								.set("createTime", DateUtils.format(new Date(), DateTimeConst.DATE_10)).save();
+						Db.update("insert into leader_income values (null,?,?,?)", leaderUser.getStr("userId"),
+								actualIncome, DateUtils.format(new Date(), DateTimeConst.DATE_10));
 
-							//获得领导奖的提成需要加到上一级用户
-							calcLeaderIncome(leaderUser, actualIncome);
-						}
+						//获得领导奖的提成需要加到上一级用户
+						calcLeaderIncome(leaderUser, actualIncome);
 					}
+					//}
 
 					generation++;
 					user = leaderUser;
